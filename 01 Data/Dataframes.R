a6d7379a1bcbbbff636cbf68dc6bbd892828da8d
Michael Hays <michaelhays1992@gmail.com>
2015-02-28 18:26:33 -0600

    made flightdf

--- 01 Data/Dataframes.R
+++ 01 Data/Dataframes.R
@@ -4,9 +4,14 @@ library("dplyr")
 library("tidyr")
 library("jsonlite")
 
-citiesdf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from CITIES"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mh33483', PASS='orcl_mh33483', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+flightcitiesdf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from CITIESTOCITIES"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mh33483', PASS='orcl_mh33483', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
-cities2df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from CITIESTOCITIES"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mh33483', PASS='orcl_mh33483', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+flightcountriesdf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from COUNTRIESTOCOUNTRIES"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mh33483', PASS='orcl_mh33483', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
-countriesdf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from COUNTRIESTOCOUNTRIES"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mh33483', PASS='orcl_mh33483', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+uscitiesdf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from USCITIES"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mh33483', PASS='orcl_mh33483', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
+tbl_df(flightcitiesdf)
+
+tbl_df(flightcountriesdf)
+
+tbl_df(uscitiesdf)
---
+++ 02 Data Wrangling/Wranglers.R
@@ -0,0 +1,6 @@
+colnames(uscitiesdf)[3] <- "DEPARTURECITY"
+colnames(flightcountriesdf)[3] <- "TOTALNUMROUTES"
+
+flightdf <- flightcitiesdf %>% inner_join(flightcountriesdf, by = c("DEPARTURECOUNTRY", "ARRIVALCOUNTRY")) %>% group_by(DEPARTURECITY) %>% summarize(TOTALDISTANCE = sum(DISTANCE)) %>% arrange(desc(TOTALDISTANCE))
+
+View(flightdf)
