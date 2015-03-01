2b5262e34f25981f9b49b9cecdad7a2aef19169b
Michael Hays <michaelhays1992@gmail.com>
2015-03-01 00:14:36 -0600

    mostly done except documentation

--- 03 Visualizations/Visualizations3.R
+++ 03 Visualizations/Visualizations3.R
@@ -3,7 +3,7 @@ flightdf3 %>% ggplot(aes(x = LNG, y = LAT)) +
   ggtitle("Map of Airports in the United States") +
   labs(x = "Longitude", y = "Latitude") +
   theme(
-    plot.title = element_text(size = 18, face = "bold", vjust = 2),
+    plot.title = element_text(size = 18, color = "blue", face = "bold", vjust = 2),
     panel.grid.major = element_line(color = "blue")
   ) +
   scale_x_continuous(breaks = seq(-180, -60, 10)) +
