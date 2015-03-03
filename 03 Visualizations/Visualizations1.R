9e0584ad4711f7feb3f72bf51337d6b0da7734c3
Michael Hays <michaelhays1992@gmail.com>
2015-03-03 16:14:36 -0600

    mi to km

--- 03 Visualizations/Visualizations1.R
+++ 03 Visualizations/Visualizations1.R
@@ -1,7 +1,7 @@
 flightdf1 %>% ggplot(aes(x = NUMFLIGHTS, y = TOTALDISTANCEOUT)) +
   geom_point() +
   ggtitle("Departed Flights Total Distance vs. \nNumber of Flights") +
-  labs(x = "Number of Flights", y = "Distance (mi)") +
+  labs(x = "Number of Flights", y = "Distance (km)") +
   theme(
     plot.title = element_text(size = 18, color = "purple", face = "bold", vjust = 2),
     panel.background = element_rect(fill = "grey75")
